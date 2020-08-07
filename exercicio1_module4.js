bd9e84f249dcb200fd39c59aec0971e416a63bd1
[ZER0.1] <53665362+ZER0-1NN@users.noreply.github.com>
2020-08-07 04:15:52 -0400

    Delete exercicio1_module4.js

--- exercicio1_module4.js
+++
@@ -1,9 +0,0 @@
-const checaIdade = (idade => new Promise((resolve, reject) => {
-    setTimeout(() => {
-        idade >= 18 ? resolve() : reject();
-    }, 2000);
-}));
-
-checaIdade(17)
-    .then(() => console.log("Maior que 18"))
-    .catch(() => console.log("Menor que 18"));
